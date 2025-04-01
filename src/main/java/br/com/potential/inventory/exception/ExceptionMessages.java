5a5f16c93862587fd4aae833495e57a243f5607a
LorranPdS <lorransantospereira@yahoo.com.br>
2025-03-31 23:06:26 -0300

    test(CategoryServiceImplTest): implemented unit test to save

--- src/main/java/br/com/potential/inventory/exception/ExceptionMessages.java
+++ src/main/java/br/com/potential/inventory/exception/ExceptionMessages.java
@@ -10,5 +10,4 @@ public class ExceptionMessages {
     public static final String CATEGORY_DESCRIPTION_MUST_BE_INFORMED = "The category description must be informed.";
     public static final String CATEGORY_ID_MUST_BE_INFORMED = "The category id must be informed.";
     public static final String CATEGORY_ID_NOT_FOUND = "There's no category for the given id";
-    public static final String INVALID_FORMAT_UUID = "The provided UUID format is not valid";
 }
