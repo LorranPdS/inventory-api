d20502891ba3f681f667c0a960ba23f2aa45e11c
LorranPdS <lorransantospereira@yahoo.com.br>
2025-05-02 21:24:39 -0300

    tests(ProductServiceImplTest): add test

---
+++ src/test/java/br/com/potential/supermarket/service/ProductServiceImplTest.java
@@ -0,0 +1,14 @@
+package br.com.potential.supermarket.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+
+}
